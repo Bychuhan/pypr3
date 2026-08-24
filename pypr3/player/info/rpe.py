45eca94c56c3f4c621795c96bd5b6c9dfeae2a7c
Bychuhan <xiechuhan2012x3@163.com>
2026-08-24 16:05:05 +0800

    feat(info): add RPEChartInfo model and parser

---
+++ pypr3/player/info/rpe.py
@@ -0,0 +1,46 @@
+from typing import IO, Any
+from io import StringIO
+
+
+from pydantic import BaseModel
+
+
+class RPEChartInfo(BaseModel):
+    Name: str = "Unknown"
+    Path: str = "Unknown"
+    Song: str | None = None
+    Picture: str | None = None
+    Chart: str | None = None
+    Level: str = "Unknown"
+    Composer: str = "Unknown"
+    Charter: str = "Unknown"
+    LastEditTime: str = "Unknown"
+    Length: float = 0
+    EditTime: float = 0
+    Group: str = "Unknown"
+
+
+class RPEChartInfoParser:
+    @staticmethod
+    def load(fp: IO[str]) -> RPEChartInfo:
+        lines = fp.readlines()
+
+        dict_data: dict[str, Any] = {}
+
+        for line in lines:
+            if ":" in line:
+                line_data = line.strip()
+                key, _, value = line_data.partition(":")
+
+                key = key.strip()
+                value = value.strip()
+
+                dict_data[key] = value
+
+        info = RPEChartInfo.model_validate(dict_data)
+
+        return info
+
+    @staticmethod
+    def loads(data: str) -> RPEChartInfo:
+        return RPEChartInfoParser.load(StringIO(data))
