0ff29844ae1f76011ab5eccdcf9ad973ef139639
Bychuhan <xiechuhan2012x3@163.com>
2026-08-24 19:49:04 +0800

    feat(info): add __init__.py for module exports

---
+++ pypr3/player/info/__init__.py
@@ -0,0 +1,15 @@
+from pypr3.player.info.rpe import RPEChartInfo, RPEChartInfoParser
+from pypr3.player.info.phira import PhiraChartInfo, PhiraChartInfoParser
+from pypr3.player.info.simphi import SimPhiChartInfo, SimPhiChartInfoParser
+from pypr3.player.info.core import ChartInfo
+
+
+__all__ = [
+    "RPEChartInfo",
+    "RPEChartInfoParser",
+    "PhiraChartInfo",
+    "PhiraChartInfoParser",
+    "SimPhiChartInfo",
+    "SimPhiChartInfoParser",
+    "ChartInfo",
+]
