514dbc6755b5dfc671fff029cdb762db9e504af8
Bychuhan <170874744+Bychuhan@users.noreply.github.com>
2026-08-27 00:30:38 +0800

    feat(renderer): add TextureRegistry to exports

--- pypr3/renderer/__init__.py
+++ pypr3/renderer/__init__.py
@@ -1,6 +1,7 @@
 from .shader import Shader
 from .core import Renderer
 from .texture import TextureConverter
+from .registry import TextureRegistry
 
 
-__all__ = ["Shader", "Renderer", "TextureConverter"]
+__all__ = ["Shader", "Renderer", "TextureConverter", "TextureRegistry"]
