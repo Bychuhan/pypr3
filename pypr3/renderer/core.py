4e653aa9b245dd12dda9bbc065f130f6cc342a6f
Bychuhan <170874744+Bychuhan@users.noreply.github.com>
2026-08-29 12:47:44 +0800

    refactor(hit)!: remove rotation, scale and anchor parameters

--- pypr3/renderer/core.py
+++ pypr3/renderer/core.py
@@ -119,16 +119,12 @@ class Renderer:
         self._texture_shader.render(mgl.TRIANGLES)
 
     def render_hit(self, screen_size: tuple[int, int], texture: mgl.Texture, x: float, y: float,
-                       w_scale: float, h_scale: float, rotation: float, anchor: tuple[float, float] = (0.5, 0.5),
                        color: tuple[float, float, float, float] = (1, 1, 1, 1), frame: float = 0, grid_size: tuple[int, int] = (1, 1),
                        texture_size: tuple[int, int] | None = None):
         self._hit_shader.set_uniform("screenSize", screen_size)
         self._hit_shader.set_uniform(
             "textureSize", texture_size or (texture.width, texture.height))
         self._hit_shader.set_uniform("position", (x, y))
-        self._hit_shader.set_uniform("scale", (w_scale, h_scale))
-        self._hit_shader.set_uniform("rotation", rotation)
-        self._hit_shader.set_uniform("anchor", anchor)
         self._hit_shader.set_uniform("color", color)
 
         self._hit_shader.set_uniform("gridSize", grid_size)
