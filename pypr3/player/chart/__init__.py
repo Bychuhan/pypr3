963e868926f384a7047ebc2c348f5494e0fc1ff2
Bychuhan <170874744+Bychuhan@users.noreply.github.com>
2026-08-30 23:30:21 +0800

    feat(chart): add ChartParser

--- pypr3/player/chart/__init__.py
+++ pypr3/player/chart/__init__.py
@@ -1,4 +1,5 @@
 from .core import Chart
+from .parser import ChartParser
 
 
-__all__ = ["Chart"]
+__all__ = ["Chart", "ChartParser"]
---
+++ pypr3/player/chart/parser.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+
+from pypr3.player.chart import Chart
+from pypr3.player.chart.phi.objs import PhiChart
+from pypr3.player.chart.rpe.objs import RpeChart
+
+
+class ChartParser:
+    @staticmethod
+    def from_dict(data: dict[Any, Any]) -> Chart:
+        chart_type: type[Chart]
+        if "META" in data:
+            chart_type = PhiChart
+        elif "formatVersion" in data:
+            chart_type = RpeChart
+        else:
+            raise ValueError(f"Unknown chart format: {list(data.keys())}")
+
+        return chart_type.from_any(data)
