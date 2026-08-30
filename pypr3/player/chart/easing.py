e8189bb5db76b147e905442879d8300911bf6477
Bychuhan <170874744+Bychuhan@users.noreply.github.com>
2026-08-30 23:31:24 +0800

    feat(chart): add easing functions

---
+++ pypr3/player/chart/easing.py
@@ -0,0 +1,125 @@
+import math
+
+
+def linear(t: float) -> float:
+    return t
+
+
+def in_sine(t: float) -> float:
+    return 1 - math.cos((t * math.pi) / 2)
+
+
+def out_sine(t: float) -> float:
+    return math.sin((t * math.pi) / 2)
+
+
+def in_out_sine(t: float) -> float:
+    return -(math.cos(math.pi * t) - 1) / 2
+
+
+def in_quad(t: float) -> float:
+    return t ** 2
+
+
+def out_quad(t: float) -> float:
+    return 1 - (1 - t) * (1 - t)
+
+
+def in_out_quad(t: float) -> float:
+    return 2 * (t ** 2) if t < 0.5 else 1 - (-2 * t + 2) ** 2 / 2
+
+
+def in_cubic(t: float) -> float:
+    return t ** 3
+
+
+def out_cubic(t: float) -> float:
+    return 1 - (1 - t) ** 3
+
+
+def in_out_cubic(t: float) -> float:
+    return 4 * (t ** 3) if t < 0.5 else 1 - (-2 * t + 2) ** 3 / 2
+
+
+def in_quart(t: float) -> float:
+    return t ** 4
+
+
+def out_quart(t: float) -> float:
+    return 1 - (1 - t) ** 4
+
+
+def in_out_quart(t: float) -> float:
+    return 8 * (t ** 4) if t < 0.5 else 1 - (-2 * t + 2) ** 4 / 2
+
+
+def in_quint(t: float) -> float:
+    return t ** 5
+
+
+def out_quint(t: float) -> float:
+    return 1 - (1 - t) ** 5
+
+
+def in_out_quint(t: float) -> float:
+    return 16 * (t ** 5) if t < 0.5 else 1 - (-2 * t + 2) ** 5 / 2
+
+
+def in_expo(t: float) -> float:
+    return 0 if t == 0 else 2 ** (10 * t - 10)
+
+
+def out_expo(t: float) -> float:
+    return 1 if t == 1 else 1 - 2 ** (-10 * t)
+
+
+def in_out_expo(t: float) -> float:
+    return 0 if t == 0 else 1 if t == 1 else 2 ** (20 * t - 10) / 2 if t < 0.5 else (2 - 2 ** (-20 * t + 10)) / 2
+
+
+def in_circ(t: float) -> float:
+    return 1 - (1 - t ** 2) ** 0.5
+
+
+def out_circ(t: float) -> float:
+    return (1 - (t - 1) ** 2) ** 0.5
+
+
+def in_out_circ(t: float) -> float:
+    return (1 - (1 - (2 * t) ** 2) ** 0.5) / 2 if t < 0.5 else (((1 - (-2 * t + 2) ** 2) ** 0.5) + 1) / 2
+
+
+def in_back(t: float) -> float:
+    return 2.70158 * (t ** 3) - 1.70158 * (t ** 2)
+
+
+def out_back(t: float) -> float:
+    return 1 + 2.70158 * ((t - 1) ** 3) + 1.70158 * ((t - 1) ** 2)
+
+
+def in_out_back(t: float) -> float:
+    return ((2 * t) ** 2 * ((2.5949095 + 1) * 2 * t - 2.5949095)) / 2 if t < 0.5 else ((2 * t - 2) ** 2 * ((2.5949095 + 1) * (t * 2 - 2) + 2.5949095) + 2) / 2
+
+
+def in_elastic(t: float) -> float:
+    return 0 if t == 0 else (1 if t == 1 else - 2 ** (10 * t - 10) * math.sin((t * 10 - 10.75) * (2 * math.pi / 3)))
+
+
+def out_elastic(t: float) -> float:
+    return 0 if t == 0 else (1 if t == 1 else 2 ** (-10 * t) * math.sin((t * 10 - 0.75) * (2 * math.pi / 3)) + 1)
+
+
+def in_out_elastic(t: float) -> float:
+    return 0 if t == 0 else (1 if t == 1 else (-2 ** (20 * t - 10) * math.sin((20 * t - 11.125) * ((2 * math.pi) / 4.5))) / 2 if t < 0.5 else (2 ** (-20 * t + 10) * math.sin((20 * t - 11.125) * ((2 * math.pi) / 4.5))) / 2 + 1)
+
+
+def in_bounce(t: float) -> float:
+    return 1 - (7.5625 * ((1 - t) ** 2) if ((1 - t) < 1 / 2.75) else (7.5625 * ((1 - t) - (1.5 / 2.75)) * ((1 - t) - (1.5 / 2.75)) + 0.75 if ((1 - t) < 2 / 2.75) else (7.5625 * ((1 - t) - (2.25 / 2.75)) * ((1 - t) - (2.25 / 2.75)) + 0.9375 if ((1 - t) < 2.5 / 2.75) else (7.5625 * ((1 - t) - (2.625 / 2.75)) * ((1 - t) - (2.625 / 2.75)) + 0.984375))))
+
+
+def out_bounce(t: float) -> float:
+    return 7.5625 * (t ** 2) if (t < 1 / 2.75) else (7.5625 * (t - (1.5 / 2.75)) * (t - (1.5 / 2.75)) + 0.75 if (t < 2 / 2.75) else (7.5625 * (t - (2.25 / 2.75)) * (t - (2.25 / 2.75)) + 0.9375 if (t < 2.5 / 2.75) else (7.5625 * (t - (2.625 / 2.75)) * (t - (2.625 / 2.75)) + 0.984375)))
+
+
+def in_out_bounce(t: float) -> float:
+    return (1 - (7.5625 * ((1 - 2 * t) ** 2) if ((1 - 2 * t) < 1 / 2.75) else (7.5625 * ((1 - 2 * t) - (1.5 / 2.75)) * ((1 - 2 * t) - (1.5 / 2.75)) + 0.75 if ((1 - 2 * t) < 2 / 2.75) else (7.5625 * ((1 - 2 * t) - (2.25 / 2.75)) * ((1 - 2 * t) - (2.25 / 2.75)) + 0.9375 if ((1 - 2 * t) < 2.5 / 2.75) else (7.5625 * ((1 - 2 * t) - (2.625 / 2.75)) * ((1 - 2 * t) - (2.625 / 2.75)) + 0.984375))))) / 2 if t < 0.5 else (1 + (7.5625 * ((2 * t - 1) ** 2) if ((2 * t - 1) < 1 / 2.75) else (7.5625 * ((2 * t - 1) - (1.5 / 2.75)) * ((2 * t - 1) - (1.5 / 2.75)) + 0.75 if ((2 * t - 1) < 2 / 2.75) else (7.5625 * ((2 * t - 1) - (2.25 / 2.75)) * ((2 * t - 1) - (2.25 / 2.75)) + 0.9375 if ((2 * t - 1) < 2.5 / 2.75) else (7.5625 * ((2 * t - 1) - (2.625 / 2.75)) * ((2 * t - 1) - (2.625 / 2.75)) + 0.984375))))) / 2
