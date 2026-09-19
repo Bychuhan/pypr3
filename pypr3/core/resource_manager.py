a83d26d67378acb662b3310865e275b3c75c6b33
Bychuhan <170874744+Bychuhan@users.noreply.github.com>
2026-09-19 21:27:09 +0800

    feat(resources): add path helper methods

--- pypr3/core/resource_manager.py
+++ pypr3/core/resource_manager.py
@@ -1,3 +1,4 @@
+import os
 from typing import IO
 from pathlib import Path
 
@@ -20,8 +21,24 @@ class ResourceManager:
         except FileNotFoundError:
             return None
 
+    def get_file_path(self, file: str) -> str:
+        return str(self.root / file)
+
+    def get_texture_path(self, file: str) -> str:
+        return str(self.root / "textures" / file)
+
+    def get_sound_path(self, file: str) -> str:
+        return str(self.root / "sounds" / file)
+
+    def get_shader_path(self, name: str) -> tuple[str, str, str]:
+        vs_path = str(self.root / "shaders" / name / "vert.glsl")
+        fs_path = str(self.root / "shaders" / name / "frag.glsl")
+        gs_path = str(self.root / "shaders" / name / "geom.glsl")
+
+        return vs_path, fs_path, gs_path
+
     def get_texture(self, file: str) -> bytes | None:
-        path = self.root / "textures" / file
+        path = self.get_texture_path(file)
 
         try:
             with open(path, "rb") as f:
@@ -30,7 +47,7 @@ class ResourceManager:
             return None
 
     def get_sound(self, file: str) -> bytes | None:
-        path = self.root / "sounds" / file
+        path = self.get_sound_path(file)
 
         try:
             with open(path, "rb") as f:
@@ -39,22 +56,20 @@ class ResourceManager:
             return None
 
     def get_shader(self, name: str) -> tuple[str | None, str | None, str | None]:
-        vs_path = self.root / "shaders" / name / "vert.glsl"
-        fs_path = self.root / "shaders" / name / "frag.glsl"
-        gs_path = self.root / "shaders" / name / "geom.glsl"
+        vs_path, fs_path, gs_path = self.get_shader_path(name)
 
         vs = None
-        if vs_path.exists():
+        if os.path.exists(vs_path):
             with open(vs_path, "r", encoding="utf-8") as f:
                 vs = f.read()
 
         fs = None
-        if fs_path.exists():
+        if os.path.exists(fs_path):
             with open(fs_path, "r", encoding="utf-8") as f:
                 fs = f.read()
 
         gs = None
-        if gs_path.exists():
+        if os.path.exists(gs_path):
             with open(gs_path, "r", encoding="utf-8") as f:
                 gs = f.read()
 
